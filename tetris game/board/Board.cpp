9abd2075e5e66fef85882977c7296316cd035bf6
zeyad zakzouk <ziyad.zakzouk@gmail.com>
2024-10-31 04:33:50 +0300

    finish the board

--- tetris game/board/Board.cpp
+++ tetris game/board/Board.cpp
@@ -33,26 +33,76 @@ bool Board::IsGameOver()
      return flag;
 }
 
-void Board::DeleteLine(int pY){
+void Board::DeleteLine(int pY)
+{
      // Moves all the upper lines one row down
- for (int j = pY; j > 0; j--){
-     for (int i = 0; i < BOARD_WIDTH; i++){
-          board[i][j] = board[i][j - 1];
-      }
-  }
+     for (int j = pY; j > 0; j--)
+     {
+          for (int i = 0; i < BOARD_WIDTH; i++)
+          {
+               board[i][j] = board[i][j - 1];
+          }
+     }
 }
 
-void Board::DeletePossibleLines (){
-  for (int j = 0; j < BOARD_HEIGHT; j++)
+void Board::DeletePossibleLines()
+{
+     for (int j = 0; j < BOARD_HEIGHT; j++)
      {
           int i = 0;
-          while(i < BOARD_WIDTH){
-            if (board[i][j] != POS_FILLED) 
-             break;
-             i++;
+          while (i < BOARD_WIDTH)
+          {
+               if (board[i][j] != POS_FILLED)
+                    break;
+               i++;
           }
-             if(i==BOARD_WIDTH)
-             DeleteLine(j);  
+          if (i == BOARD_WIDTH)
+               DeleteLine(j);
+     }
+}
+
+bool Board::IsFreeBlock(int pX, int pY)
+{
+     if (board[pX][pY] == POS_FILLED)
+          return false;
+
+     return true;
+}
+
+int Board::GetYPosInPixels(int pPos)
+{
+     return ((BOARD_POSITION - (BLOCK_SIZE * (BOARD_HEIGHT / 2))) + (pPos * BLOCK_SIZE));
+}
+int Board::GetXPosInPixels(int pPos)
+{
+     return ((screenHeight - (BLOCK_SIZE * BOARD_WIDTH)) + (pPos * BLOCK_SIZE));
+}
+
+bool Board::IsPossibleMovement(int pX, int pY, int pPiece, int pRotation)
+{
+     for (int i1 = pX, i2 = 0; i1 < pX + PIECE_BLOCKS; i1++, i2++)
+     {
+          for (int j1 = pY, j2 = 0; j1 < pY + PIECE_BLOCKS; j1++, j2++)
+          {
+               // Check if the piece is outside the limits of the board
+               if (i1 < 0 ||
+                   i1 > BOARD_WIDTH - 1 ||
+                   j1 > BOARD_HEIGHT - 1)
+               {
+                    if (pieces->getBlockType(pPiece, pRotation, j2, i2) != 0)
+                         return false;
+               }
+
+               // Check if the piece have collisioned with a block already stored in the map
+               if (j1 >= 0)
+               {
+                    if ((pieces->getBlockType(pPiece, pRotation, j2, i2) != 0) &&
+                        (!IsFreeBlock(i1, j1)))
+                         return false;
+               }
+          }
      }
 
+     // No collision
+     return true;
 }
